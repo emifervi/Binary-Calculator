35d7496e6221eceee07d4d7dee7e1e823e167ba3
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-10-11 21:27:05 -0500

    Initial Commit

---
+++ Binary-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binary-Calculator
+//
+//  Created by Ricardo González Castillo on 10/11/19.
+//  Copyright © 2019 Binary-Boiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

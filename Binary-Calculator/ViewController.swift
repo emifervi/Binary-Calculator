0041fca24010019c9fa823e6a37f00d6408ece96
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-10-12 15:26:52 -0500

    Allow to append text when a click is perform in the digits buttons

--- Binary-Calculator/ViewController.swift
+++ Binary-Calculator/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var digitsStackView: UIStackView!
+
+    var digitButtons : [UIButton]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // loadDigitButtons()
     }
 
+    func loadDigitButtons() {
+        let innerStackViews = digitsStackView.subviews as! [UIStackView]
+        let lastIndex = innerStackViews.count - 1
+        for index in 0..<lastIndex {
+            let innerDigits = innerStackViews[index].subviews as! [UIButton]
+            digitButtons += innerDigits[0...2]
+        }
+        digitButtons.append(
+            innerStackViews[lastIndex].subviews[1] as! UIButton
+        )
+    }
+
+    @IBAction func addDigit(_ sender: UIButton) {
+        var number = numberTextField.text ?? ""
+        if number == "0" {
+            number = ""
+        }
+        numberTextField.text = number + sender.titleLabel!.text!
+    }
 
 }
 

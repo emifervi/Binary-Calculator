04df2ec070d89e91ea9c16adb5468f7a8b595afe
JALVITO <lizvizjuan@gmail.com>
2019-11-20 11:34:29 -0600

    Add instructions and credits screen

---
+++ Binary-Calculator/ViewControllerInstrucciones.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllerInstrucciones.swift
+//  Binary-Calculator
+//
+//  Created by Juan Lizarraga on 11/20/19.
+//  Copyright © 2019 Binary-Boiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerInstrucciones: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeInstructions(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
